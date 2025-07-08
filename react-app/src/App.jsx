6d4e28a695848f3ac64da647404632a5faef9c16
AditiTechiee <2023bcamafsaditee13674@poornima.edu.in>
2025-07-08 21:45:44 +0530

    Fixed routes in App.js

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 
@@ -7,10 +7,12 @@ function App() {
   return (
     <Router>
       <Routes>
-       
+        {/* Redirect root ("/") to /signup */}
+        <Route path="/" element={<Navigate to="/signup" />} />
+        
+        {/* Actual routes */}
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} /> {/* ✅ Fixed this line */}
-
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
